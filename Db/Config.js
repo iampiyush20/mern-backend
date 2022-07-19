bf0e76c56170bc453bf2ecf82f6deaf5ab32a463
piyush <piyushvijay20002@gmail.com>
2022-07-19 16:11:11 +0530

    make it better

--- Db/Config.js
+++ Db/Config.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://piyushdb:9667751125@cluster0.la410e4.mongodb.net/?retryWrites=true&w=majority",()=>{
+mongoose.connect("mongodb+srv://piyushdb:9667751125@cluster0.la410e4.mongodb.net/?retryWrites=true&w=majority+",{
+    useNewUrlParser:true, 
+    useUnifiedTopology:true
+},()=>{
     console.log('db connected')
 })
