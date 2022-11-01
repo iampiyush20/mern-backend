a8f68e62a8eb3910826b477c27792bd03380b0ee
piyush <piyushvijay20002@gmail.com>
2022-11-01 09:04:55 +0530

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json())
 
 const PORT=process.env.PORT || 5000;
-
+// sdfrr
 const User=require('./Db/User');
 app.use(cors({'Access-Control-Allow-Origin': '*'}));
 app.post('/api/register',async(req,res)=>{
